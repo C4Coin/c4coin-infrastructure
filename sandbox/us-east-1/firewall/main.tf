292c89406cad66b8d4ea81ea12f2147c3babb846
PWang <pwang@essextec.com>
2018-07-09 13:50:39 -0400

    Create firewall rules in sandbox

---
+++ sandbox/us-east-1/firewall/main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  project     = "${var.project}"
+  region = "${var.region}"
+}
+
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-sandbox-c4coin"
+    prefix = "/us-east-1/firewall"
+  }
+}
+
+
+resource "google_compute_firewall" "allow-icmp" {
+  name = "${var.vpc_name}-allow-icmp"
+  network = "${var.vpc_name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+
+
+
+
